93fd5ca9516c9583ec9ffd889802cb9a2e65952a
Meili Triantafyllidi <meili@uberresearch.com>
2020-04-13 13:29:52 +0200

    move static assets to static directory

--- static/script.js
+++ static/script.js
@@ -1,7 +1,13 @@
+var global = {
+  ws: null,
+  username: null,
+  ws_url: ws_url  // from base template 
+};
 var setnamebutton = document.getElementById("setname");
 var sendinvitationbutton = document.getElementById("sendinvitation");
 setnamebutton.addEventListener('click', function(e) {
   e.preventDefault();
+  global.username = $('#nickname')[0].value; 
   $('#host-1').addClass('animated fadeOut faster');
   $('#host-2').addClass('animated fadeIn fast');
   $('#host-2').toggleClass('active');
@@ -12,7 +18,15 @@ setnamebutton.addEventListener('click', function(e) {
    }, 800);
 })
 
+
 sendinvitationbutton.addEventListener('click', function(e) {
+  ws = new WebSocket(ws_url);
+  ws.onopen = function(){
+    ws.send(JSON.stringify({"username": global.username}));
+  }
+    ws.onmessage = function(event) {
+    var friend_url = event.data; /// here is the friend url
+  };
   e.preventDefault();
    $("#sendinvitation p").attr('data-title', "Αποστέλλεται...");
   console.log("ToDo: send invitation");
